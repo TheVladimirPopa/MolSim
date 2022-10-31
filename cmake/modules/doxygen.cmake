98ae08cb90d4af5be90c234f00310c8b05d50de3
Leo Pahl <leo.pahl@web.de>
2022-10-31 10:46:54 +0100

    chore: add doxygen support to cmake

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,40 @@
 # make doc_doxygen optional if someone does not have / like doxygen
 
+
 # TODO: create CMake build option for the target.
+option(BUILD_DOC "Enable building of doxygen documentation" ON)
 
 # TODO: Add a custom target for building the documentation.
+
+FIND_PACKAGE(Doxygen)
+
+IF (BUILD_DOC)
+
+    IF (DOXYGEN_FOUND)
+        MESSAGE(STATUS "Doxygen found: ${DOXYGEN_EXECUTABLE} -- ${DOXYGEN_VERSION}")
+        IF (DOXYGEN_VERSION VERSION_LESS 1.9.1)
+            MESSAGE(WARNING "Only tested with doxygen version 1.9, your version(${DOXYGEN_VERSION}) is older and may not work. Consider updating.")
+        ENDIF ()
+        set(DOXYGEN_IN ${CMAKE_HOME_DIRECTORY}/Doxyfile)
+        set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+        # Set parameters for Doxygen-file
+        set(DOXYGEN_INPUT "../src \
+                ../README.md")
+        set(DOXYGEN_EXCLUDE "../src/outputWriter/vtk-unstructured.cpp \
+                ../src/outputWriter/vtk-unstructured.h \
+                ../src/outputWriter/vtk-unstructured.xsd")
+        set(DOXYGEN_MAIN_PAGE ../README.md)
+
+        # Write the parameters to the doxygen-file
+        configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+        message(STATUS "Writing of doxygen-file done")
+
+        # Add doc_doxygen as a target to
+        add_custom_target(doc_doxygen ALL
+                COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                VERBATIM)
+    ELSE ()
+        MESSAGE(WARNING "Doxygen wasn't found make sure to install doxygen, additionally you may need to install graphiz")
+    ENDIF ()
+ENDIF ()
