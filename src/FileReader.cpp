2eccf93380643b76e5166cdc02bfe5711a999717
Leo Pahl <leo.pahl@web.de>
2022-11-01 11:52:31 +0100

    feat: Use std::vector in fileReader

--- src/FileReader.cpp
+++ src/FileReader.cpp
@@ -16,7 +16,7 @@ FileReader::FileReader() = default;
 
 FileReader::~FileReader() = default;
 
-void FileReader::readFile(std::list<Particle> &particles, char *filename) {
+void FileReader::readFile(std::vector<Particle> &particles, char *filename) {
   std::array<double, 3> x;
   std::array<double, 3> v;
   double m;
@@ -40,6 +40,7 @@ void FileReader::readFile(std::list<Particle> &particles, char *filename) {
     getline(input_file, tmp_string);
     std::cout << "Read line: " << tmp_string << std::endl;
 
+    particles.reserve(num_particles);
     for (int i = 0; i < num_particles; i++) {
       std::istringstream datastream(tmp_string);
 
