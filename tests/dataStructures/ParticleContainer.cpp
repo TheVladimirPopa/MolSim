8af2c94567f2c55cbbf19f7be706795cea66ad94
Jakob Semmler <j.semmler@tum.de>
2022-11-10 15:42:41 +0100

    feat: Add cmake files for googletest

---
+++ tests/dataStructures/ParticleContainer.cpp
@@ -0,0 +1,25 @@
+#include "../../src/dataStructures/Particle.h"
+#include "../../src/dataStructures/ParticleContainer.h"
+#include "gtest/gtest.h"
+
+TEST(ForEachTestSuite, AppliesToAllElements) {
+  ParticleContainer particleContainer{};
+  /*
+  std::array<double, 3> x_arg = {0.0, 0.0, 0.0};
+  std::array<double, 3> v_arg = {0.0, 0.0, 0.0};
+  double m_arg = 1.0;
+  int type = 0;
+
+  for (int i = 0; i < 50; i++) {
+    Particle p{x_arg, v_arg, m_arg, type};
+    particleContainer.push_back(p);
+  }*/
+
+  int ct = 0;
+  std::function<void(Particle &)> count =
+      [&ct](auto & p){ ct += 1; };
+
+  particleContainer.forEach(count);
+
+  ASSERT_EQ(ct, 50);
+}
