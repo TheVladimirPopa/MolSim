9105bebc5bffe0782d20f2e2a7726023c3992121
Leo Pahl <leo.pahl@web.de>
2022-11-11 18:16:02 +0100

    feat: add better help screen

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -13,14 +13,12 @@
 #include "outputWriter/IWriter.h"
 #include "outputWriter/VTKWriter.h"
 
-/**
- * Usage: ./MolSim inputfile end_time delta_t
- * @param argc Has to be 4 in order to run
- * @param argsv argsv[1] = inputfilePath;
- *  argsv[2]= end_time;
- *  argsv[3]= delta_t (stepwidth)
- * @return 0 iff the simulation and I/O was successful
- */
+void printHelp();
+void printUsage();
+enum simTypes { Single, Cuboid };
+const std::map<std::string, simTypes> simTypeStrings{
+    {"single", simTypes::Single}, {"cuboid", simTypes::Cuboid}};
+
 int main(int argc, char *argsv[]) {
   std::cout << "Hello from MolSim for PSE!" << std::endl;
   Simulation simulation{};
@@ -36,7 +34,7 @@ int main(int argc, char *argsv[]) {
       {"end-time", required_argument, nullptr, 'e'},
       {"delta-t", required_argument, nullptr, 'd'},
       {"write-frequency", required_argument, nullptr, 'w'},
-      {"--help", no_argument, nullptr, 'h'},
+      {"help", no_argument, nullptr, 'h'},
       {nullptr, 0, nullptr, 0}};
   while ((opt = getopt_long(argc, argsv, "o:f:ie:d:w:h", long_options,
                             nullptr)) != -1) {
@@ -63,11 +61,15 @@ int main(int argc, char *argsv[]) {
         simulation.setWriteOutFrequency(std::stoi(optarg));
         break;
       }
-      case '?':
-      case 'h':
-      default: {
-        std::cout << "HELP" << std::endl;
+      case 'h': {
+        printHelp();
         return 0;
+      }
+      case '?':
+
+      default: {
+        printUsage();
+        return 1;
       }
     }
   }
@@ -83,5 +85,51 @@ int main(int argc, char *argsv[]) {
   std::cout << "Output written. Terminating..." << std::endl;
   return 0;
 }
+void printUsage() {
+  std::cout << "Usage\n"
+               "        ./Molsim (-i|-f <inputfile>) [-o <outputfile>] [-e "
+               "<endtime>]\n"
+               "                 [-d <deltaT>] [-w <iteration-count>] \n"
+               "For more information run ./Molsim -h"
+            << std::endl;
+}
 
-
+void printHelp() {
+  std::cout
+      << "Usage\n"
+         "        ./Molsim (-i|-f <inputfile>) [-o <outputfile>] [-e "
+         "<endtime>]\n"
+         "                 [-d <deltaT>] [-w <iteration-count>] \n"
+         "\n"
+         "OPTIONS:\n"
+         "        -o <filepath>, --output-name=<filepath>\n"
+         "                Use the given <filepath> as the path for the "
+         "outputfiles(iteration number and file-ending are added "
+         "automatically)\n"
+         "                If not specified \"MD_vtk\" is used\n"
+         "\n"
+         "        -f <filepath>, --input-file=<filepath>\n"
+         "                Use the file at the <filepath> as an input.\n"
+         "                The -f option is mutally exclusive with -i.\n"
+         "\n"
+         "        -i, --interactive\n"
+         "                Allows to interactively set up the simulation "
+         "instead of reading in a inputfile.\n"
+         "                The -i option is mutally exclusive with -f.\n"
+         "\n"
+         "        -e <endtime>, --end-time=<endtime>\n"
+         "                The time until the simulation is run (default is "
+         "1000).\n"
+         "\n"
+         "        -d <deltaT>, --delta-t=<deltaT>\n"
+         "                The timestep by which the time gets increased in "
+         "every iteration (default is 0.014).\n"
+         "\n"
+         "        -w <iteration-count>, --write-frequency=<iteration-count>\n"
+         "                Every <iteration-count>nth iteration the particles "
+         "get written to a file (default is 10).\n"
+         "\n"
+         "        -h, --help\n"
+         "                Prints this help screen."
+      << std::endl;
+}
