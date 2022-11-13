882ed32bcd61e97c11120b12851aae976da19b6f
Jakob Semmler <j.semmler@tum.de>
2022-11-13 13:37:51 +0100

    refactor: Set defaults params in Particle constructor for cleaner code

--- tests/utils/TestUtils.h
+++ tests/utils/TestUtils.h
@@ -7,18 +7,12 @@ namespace TestUtils {
    * Generates container for the ParticleContainer test suite.
    */
   static ParticleContainer makeContainer(unsigned size) {
-    std::array<double, 3> x_arg = {0.0, 0.0, 0.0};
-    std::array<double, 3> v_arg = {0.0, 0.0, 0.0};
-    double m_arg = 1.0;
-    int type = 0;
-
     ParticleContainer particle_container{};
     for (unsigned i = 0; i < size; i++) {
-      Particle p{x_arg, v_arg, m_arg, type};
+      Particle p{};
       particle_container.push_back(p);
     }
 
     return particle_container;
   }
-}
-
+}  // namespace TestUtils
