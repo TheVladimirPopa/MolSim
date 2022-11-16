f817f4fcfb1568c5db8aaed1dfa365d232ff9e06
Jakob Semmler <j.semmler@tum.de>
2022-11-16 21:51:14 +0100

    refactor: replace std::function with templates (performance improvement)

--- src/dataStructures/ParticleContainer.h
+++ src/dataStructures/ParticleContainer.h
@@ -2,7 +2,6 @@
 // Created by leo on 01.11.22.
 //
 #pragma once
-#include <functional>
 #include <vector>
 
 #include "Particle.h"
@@ -14,7 +13,13 @@ class ParticleContainer : public std::vector<Particle> {
    * @param unaryFunction A function taking a particle reference and returning
    * nothing
    */
-  void forEach(std::function<void(Particle &)> &unaryFunction);
+  template <typename F>
+  void forEach(F unaryFunction) {
+    for (size_t i = 0; i < size(); ++i) {
+      unaryFunction(this->operator[](i));
+    }
+  }
+
   /**
    * Applies a function to all particle pairs in the container
    * @param binaryFunction A function taking two particle references and
@@ -23,5 +28,12 @@ class ParticleContainer : public std::vector<Particle> {
    * (p1,p2), it won't get applied to (p2,p1). Also pairs with the same particle
    * are not considered
    */
-  void forEachPair(std::function<void(Particle &, Particle &)> &binaryFunction);
+  template <typename F>
+  void forEachPair(F binaryFunction) {
+    for (size_t i = 0; i < size(); ++i) {
+      for (size_t j = i + 1; j < size(); ++j) {
+        binaryFunction(this->operator[](i), this->operator[](j));
+      }
+    }
+  }
 };
