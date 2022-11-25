f8f2cf7a6f2ee4f91a7ec2939562d5dea6b9cf73
Jakob Semmler <j.semmler@tum.de>
2022-11-25 12:55:33 +0100

    style: rename x to sig_len to comply with feedback

--- src/model/LennardJonesModel.cpp
+++ src/model/LennardJonesModel.cpp
@@ -6,14 +6,14 @@ using ArrayUtils::L2Norm;
 void LennardJonesModel::addForces(Particle &p1, Particle &p2) const {
   // Calculate (sigma/length)^6
   double length = L2Norm(p1.x - p2.x);
-  double x = sigma / length;  // (sigma/length)^1
-  x = x * x * x;              // (sigma/length)^3
-  x = x * x;                  // (sigma/length)^6
+  double sig_len = sigma / length;        // (sigma/length)^1
+  sig_len = sig_len * sig_len * sig_len;  // (sigma/length)^3
+  sig_len = sig_len * sig_len;            // (sigma/length)^6
 
   // This is the force formula from worksheet 2 slightly reformulated for better
   // numerical accuracy.
-  auto force = ((-24 * epsilon) / (length * length)) * (x * (1 - (2 * x))) *
-               (p1.x - p2.x);
+  auto force = ((-24 * epsilon) / (length * length)) *
+               (sig_len * (1 - (2 * sig_len))) * (p1.x - p2.x);
 
   p1.f = p1.f + force;
   p2.f = p2.f - force;
