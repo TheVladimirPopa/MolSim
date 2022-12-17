c3f7d88891832a7969598e1b0e5e08e468aecbbd
Leo Pahl <leo.pahl@web.de>
2022-12-17 10:15:15 +0100

    docs: add description for tests

--- tests/dataStructures/ThermostatTest.cpp
+++ tests/dataStructures/ThermostatTest.cpp
@@ -1,11 +1,13 @@
 #include "dataStructures/VectorContainer.h"
 #include "gtest/gtest.h"
-#include "inputReader/ParticleGenerator.h"
 #include "model/Thermostat.h"
 #include "utils/ArrayUtils.h"
 
 using v3d = std::array<double, 3>;
 
+/**
+ * Make sure the total kinetic energy of an system is calculated correctly
+ */
 TEST(Thermostat, ComputeEnergy) {
   VectorContainer container{};
   container.emplace_back(v3d{0., 0., 0.}, v3d{1., 2., 2.}, 3.0, 1);
@@ -21,6 +23,9 @@ TEST(Thermostat, ComputeEnergy) {
   EXPECT_EQ(thermostat.computeEnergy(), 13.5 + 25. + 7.5);
 }
 
+/**
+ * Make sure the temperature is calculated correctly
+ */
 TEST(Thermostat, ComputeTemperature) {
   VectorContainer container{};
   container.emplace_back(v3d{0., 0., 0.}, v3d{1., 2., 2.}, 3.0,
@@ -36,6 +41,11 @@ TEST(Thermostat, ComputeTemperature) {
   Thermostat thermostat2d{container, 0., 0., 0., 0, 2};
   EXPECT_EQ(thermostat2d.getCurrentTemperature(), 2. * 45. / (3 * 2));
 }
+
+/**
+ * Make sure the velocities of all particles get scaled to reach target
+ * temperature
+ */
 TEST(Thermostat, VelocitiesScaledCorrectly) {
   std::array<v3d, 3> velocities{v3d{1., 1., 1.}, v3d{0., 10., 0.},
                                 v3d{1., 0., 0.}};
@@ -74,6 +84,9 @@ TEST(Thermostat, VelocitiesScaledCorrectly) {
   }
 }
 
+/**
+ * Make sure the temperature change is below the maxTemperatureChange
+ */
 TEST(Thermostat, StepwiseCoolingAndHeating) {
   VectorContainer container{};
   container.emplace_back(v3d{0., 0., 0.}, v3d{0., 4., 3.}, 2.0, 1);
@@ -103,6 +116,10 @@ TEST(Thermostat, StepwiseCoolingAndHeating) {
   EXPECT_DOUBLE_EQ(thermostat40.getCurrentTemperature(), 40);
 }
 
+/**
+ * Test if the initialTemperature is set correctly when the particles are
+ * already moving
+ */
 TEST(Thermostat, InitialTemperature) {
   VectorContainer container{};
   container.emplace_back(v3d{0., 0., 0.}, v3d{0., 1., 0.}, 2.0, 1);
@@ -116,6 +133,10 @@ TEST(Thermostat, InitialTemperature) {
   EXPECT_DOUBLE_EQ(thermostat.getCurrentTemperature(), 50.);
 }
 
+/**
+ * Test if the initial temperature is set correctly, when all particles have
+ * zero velocity
+ */
 TEST(Thermostat, InitialTemperatureBrownian) {
   VectorContainer container{};
   container.emplace_back(v3d{0., 0., 0.}, v3d{0., 0., 0.}, 2.0, 1);
