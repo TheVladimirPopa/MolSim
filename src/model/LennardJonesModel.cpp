c890524e58f5124e1a2674b897fb365de5e5c97c
Leo Pahl <leo.pahl@web.de>
2022-12-19 15:07:59 +0100

    perf: rework calculation of LennardJonesModel

--- src/model/LennardJonesModel.cpp
+++ src/model/LennardJonesModel.cpp
@@ -1,22 +1,21 @@
 #include "LennardJonesModel.h"
 
 #include "utils/ArrayUtils.h"
-using ArrayUtils::L2Norm;
+using ArrayUtils::dotProduct;
 
 void LennardJonesModel::addForces(Particle &p1, Particle &p2) {
-  double length = L2Norm(p1.x - p2.x);
+  double length = dotProduct(p1.x - p2.x);
   ++comparisons;
   if (length >= cutOffRadius) return;
   ++hits;
 
-  // Calculate (sigma/length)^6
-  double sig_len = sigma / length;        // (sigma/length)^1
-  sig_len = sig_len * sig_len * sig_len;  // (sigma/length)^3
-  sig_len = sig_len * sig_len;            // (sigma/length)^6
+  // Calculate (sigma⁶/length³)
+  double sig_len = sigma * sigma / length;  // (sigma²/length)
+  sig_len *= sig_len * sig_len;             // (sigma²/length)³
 
   // This is the force formula from worksheet 2 slightly reformulated for better
   // numerical accuracy.
-  auto force = ((-24 * epsilon) / (length * length)) * (sig_len * (1 - (2 * sig_len))) * (p1.x - p2.x);
+  auto force = ((-24 * epsilon) / length) * (sig_len * (1 - (2 * sig_len))) * (p1.x - p2.x);
 
   p1.f = p1.f + force;
   p2.f = p2.f - force;
