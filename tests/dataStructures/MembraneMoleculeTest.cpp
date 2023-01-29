899481e4944c349208e1d5ef5ccfe5bc4cc268f3
Jakob Semmler <j.semmler@tum.de>
2023-01-29 14:33:08 +0100

    tests: add tests to membrane ensure forces are applied correctly

--- tests/dataStructures/MembraneMoleculeTest.cpp
+++ tests/dataStructures/MembraneMoleculeTest.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "dataStructures/LinkedCellsContainer.h"
 #include "dataStructures/MembraneStructure.h"
 #include "gtest/gtest.h"
@@ -5,6 +7,7 @@
 using ArrayUtils::L2Norm;
 #include "inputReader/ParticleGenerator.h"
 using MembraneInput = ParticleGeneration::membrane;
+using SphereInput = ParticleGeneration::sphere;
 using v3d = std::array<double, 3>;
 
 /**
@@ -20,6 +23,21 @@ MembraneInput makeSampleMembraneInput() {
   mem.position = {0, 0, 0};
   mem.velocity = {0, 0, 0};
   return mem;
+}
+
+/**
+ * @return A membrane struct that can be used to generate the actual body
+ */
+SphereInput makeSampleSphereInput() {
+  SphereInput sphere{};
+  sphere.position = {30, 30, 30};
+  sphere.radius = 3;
+  sphere.dimension = 3;
+  sphere.velocity = {0., 1., 2.};
+  sphere.distance = {1.2};
+  sphere.mass = {1.0};
+  sphere.type = 1;
+  return sphere;
 }
 
 /**
@@ -119,6 +137,8 @@ TEST(MembraneStructure, RepulsiveForces) {
 
   auto& particle1 = cont->getParticlesRef()[0];
   auto& particle2 = cont->getParticlesRef()[10];
+  ASSERT_EQ(particle1.getId(), 0) << "Membrane applied wrong id";
+  ASSERT_EQ(particle2.getId(), 10) << "Membrane applied wrong id";
 
   auto x1 = v3d{10, 10, 10};
   particle1.setX(x1);
@@ -126,9 +146,102 @@ TEST(MembraneStructure, RepulsiveForces) {
   auto x2 = v3d{11, 10, 10};
   particle2.setX(x2);
 
-  cont->getStructureVectorRef().back().applyForce(particle1, particle2, 0, 10);
+  cont->getStructureVectorRef().back().applyForce(particle1, particle2);
 
   auto expectedForce1 = v3d{-120., 0.0, 0.0};
   ASSERT_EQ(particle1.getF(), expectedForce1);
   ASSERT_EQ(particle2.getF(), (-1.0) * expectedForce1);
+}
+
+/**
+ * Tests if repulse and attracting forces are applied based on particle distance.
+ */
+TEST(MembraneStructure, ForcesBig) {
+  auto mem = makeSampleMembraneInput();
+  auto cont = makeSampleContainer();
+
+  mem.dimension = {6, 6, 1};
+  mem.distance = 1.0;
+  mem.bondLength = 1.0;
+
+  ParticleGeneration::addSphereToParticleContainer(*cont, makeSampleSphereInput());
+  ParticleGeneration::addMembraneToParticleContainer(*cont, mem);
+  auto& membrane = cont->getStructureVectorRef().front();
+
+  // Check whether all particles are actually repulsed when they are supposed to
+  auto testParticlePair = [&](std::array<unsigned long, 3> pos1, std::array<unsigned long, 3> pos2) {
+    auto& p1 = membrane.getParticleByPosition(pos1);
+    auto& p2 = membrane.getParticleByPosition(pos2);
+
+    if (&p1 == &p2) return;
+
+    // Move particles to location
+    auto orig1 = p1.getX();
+    auto orig2 = p2.getX();
+
+    p1.setX({15, 0, 0});
+    p2.setX({17.5, 0, 0});
+
+    // Apply forces
+    p1.updateForces();
+    p2.updateForces();
+    membrane.applyForce(p1, p2);
+
+    p1.setX(orig1);
+    p2.setX(orig2);
+
+    if (ArrayUtils::L2Norm(orig2 - orig1) < 1.5) {
+      // Neighbours must attract
+      ASSERT_GT(p1.getF()[0], 0) << "Neighbours should attract each other. Ids: " << p1.getId() << ", " << p2.getId();
+    } else {
+      // Non-neighbours must repel forces
+      ASSERT_EQ(std::abs(p1.getF()[0]), 0) << "No force expected for >2^1/6 diff " << p1.getId() << ", " << p2.getId();
+    }
+  };
+
+  // Check each possible combination of particles of the membrane interacting
+  for (int i = 0; i < mem.dimension[0]; i++) {
+    for (int j = 0; j < mem.dimension[1]; j++) {
+      for (int k = 0; k < mem.dimension[0]; k++) {
+        for (int l = 0; l < mem.dimension[1]; l++) {
+          testParticlePair({static_cast<unsigned long>(i), static_cast<unsigned long>(j), 0},
+                           {static_cast<unsigned long>(k), static_cast<unsigned long>(l), 0});
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Checks ids, iterators and applies one repulsive test force in the presence of other particles in the container.
+ */
+TEST(MembraneStructure, DisplacedIds) {
+  auto mem = makeSampleMembraneInput();
+  auto cont = makeSampleContainer();
+
+  mem.dimension = {50, 50, 1};
+  size_t memSize = mem.dimension[0] * mem.dimension[1] * mem.dimension[2];
+
+  // Generate sphere and find out where it ends
+  ParticleGeneration::addSphereToParticleContainer(*cont, makeSampleSphereInput());
+  size_t sphereParticleIndex = cont->getParticlesRef().back().getId();
+
+  // Add membrane and find sphere in the resized particle vector
+  ParticleGeneration::addMembraneToParticleContainer(*cont, mem);
+  auto& sphereParticle = cont->getParticlesRef()[sphereParticleIndex];
+
+  // Test ids, iterators, etc.
+  auto& membrane = cont->getStructureVectorRef().front();
+  auto& firstParticle = *membrane.begin();
+  auto& lastParticle = *(membrane.end() - 1);
+
+  ASSERT_NE(&firstParticle, &lastParticle) << "Membrane structure is faulty";
+  ASSERT_NE(&firstParticle, &sphereParticle) << "Membrane structure is faulty";
+  ASSERT_NE(&lastParticle, &sphereParticle) << "Membrane structure is faulty";
+  ASSERT_EQ(sphereParticle.getId() + 1, firstParticle.getId()) << "Membrane structure is faulty or wrong ids";
+  ASSERT_EQ(sphereParticle.getId() + memSize, lastParticle.getId()) << "Membrane structure is faulty or wrong ids";
+  ASSERT_EQ(membrane.size(), memSize) << "Membrane is miss-constructed";
+  size_t size{0};
+  for (auto begin = membrane.begin(); begin != membrane.end(); begin++) size++;
+  ASSERT_EQ(size, membrane.size()) << "Membrane iterators are off";
 }
