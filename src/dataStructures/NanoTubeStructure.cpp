6ed01a31a5c2e0aeefde8dee7788e65bccafffa0
Jakob Semmler <j.semmler@tum.de>
2023-02-01 13:38:29 +0100

    fix: apply nanotube forces correctly through lines

--- src/dataStructures/NanoTubeStructure.cpp
+++ src/dataStructures/NanoTubeStructure.cpp
@@ -4,12 +4,19 @@ NanoTubeStructure::NanoTubeStructure(std::array<size_t, 3> dimensions, double st
                                      double cutOff, std::vector<Particle>& particles)
     : MembraneStructure(dimensions, stiffness, bondLength, cutOff, particles) {}
 
-void NanoTubeStructure::applyForce(Particle& p1, Particle& p2) {
-  size_t diff = p1.getId() < p2.getId() ? p2.getId() - p1.getId() : p1.getId() - p2.getId();
+void NanoTubeStructure::applyForce(Particle& p1, Particle& p2)  {
+  if (p2.getId() < p1.getId()) return applyForce(p2, p1);
+  size_t diff = p2.getId() - p1.getId();
+  size_t width = dimensions[dimWidth];
 
-  if (diff == 1 || diff == dimensions[dimWidth]) return MembraneStructure::applyDirectForce(p1, p2);
+  bool isNotAtRightBorder = (p1.getId() - startIndex) % width + 1 != width;
+  bool isNotAtLeftBorder = (p1.getId() - startIndex) % width != 0;
 
-  if (diff == dimensions[dimWidth] - 1 || diff == dimensions[dimWidth] + 1) return applyDiagonalForce(p1, p2);
+  if ((diff == 1 && isNotAtRightBorder) || diff == width || (!isNotAtLeftBorder && diff == (width - 1))) return applyDirectForce(p1, p2);
+
+  if (((diff == width - 1) && isNotAtLeftBorder) || ((diff == width + 1) && isNotAtRightBorder) ||
+      (!isNotAtLeftBorder && diff == (width + width - 1)) || (!isNotAtRightBorder && diff == 1))
+    return applyDiagonalForce(p1, p2);
 
   return applyRepulsiveForce(p1, p2);
 }
